5e74bf449b3026c72030a465fa608f932934003b
21 <3170549226@qq.com>
2022-04-28 18:54:25 +0800

    条件装配,静态资源访问

---
+++ 08-SpringBoot-05-Conditional/src/main/java/com/fc/entity/Bike.java
@@ -0,0 +1,10 @@
+package com.fc.entity;
+
+import lombok.Data;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Data
+
+public class Bike {
+}
---
+++ 08-SpringBoot-06-StaticResources/src/main/java/com/fc/controller/TestController.java
@@ -0,0 +1,13 @@
+//package com.fc.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class TestController {
+//
+//    @RequestMapping("/")
+//    public String testStatic() {
+//        return "forward:/test/testIndex.html";
+//    }
+//}
